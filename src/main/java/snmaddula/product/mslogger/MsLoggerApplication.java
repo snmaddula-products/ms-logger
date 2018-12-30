a98ae86da21d466d7c5e1add175a0c47d41b5813
snmaddula <snmaddula@outlook.com>
2018-12-31 00:28:16 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/product/mslogger/MsLoggerApplication.java
@@ -0,0 +1,14 @@
+package snmaddula.product.mslogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLoggerApplication.class, args);
+	}
+
+}
+
